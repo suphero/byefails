e01a55bd899e73ceffba2bd395ae543fa82bbab6
suphero <harunsokullu@gmail.com>
2019-12-09 09:58:04 +0300

    allow credential

--- src/lambda/calculate.js
+++ src/lambda/calculate.js
@@ -30,7 +30,8 @@ exports.handler = async (event, _context, _callback) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+    'Access-Control-Allow-Credentials': true
   };
 
   if (event.httpMethod === "POST") {
